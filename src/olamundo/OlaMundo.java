2f0c868f2337140137ab088a465e18af8154e110
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2024-09-28 21:11:06 -0300

    adicionando ola mundo em Java

---
+++ src/olamundo/OlaMundo.java
@@ -0,0 +1,12 @@
+package olamundo;
+
+
+public class OlaMundo {
+
+    public static void main(String[] args) {
+        system.out.printIn("Olá Mundo!");
+        float v=9;
+        system.out.printIn("O quadrado é" v*v);
+    }
+    
+}
